76a1720fe2b23c435c46c684877a397945e8bf3f
sp_3141 <07shourya.pratap@gmail.com>
2026-03-20 22:37:04 +0530

    Added add function

--- SLL.py
+++ SLL.py
@@ -6,3 +6,20 @@ class Node:
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
+        
+    def add(self, val):
+        newNode = Node(val)
+        if not self.head:
+            self.head = newNode
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = newNode
+            
+def main():
+    LL = SinglyLinkedList()
+    LL.add(1)
+
+if __name__ == "__main__":
+    main()
