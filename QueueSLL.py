3957e8d35382eaeedca50ee0e34ac805077a423c
sp_3141 <07shourya.pratap@gmail.com>
2026-03-21 22:05:44 +0530

    Updated QueueSLL.py

---
+++ QueueSLL.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        
+class QueueSLL:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def enqueue(self, val):
+        pass
+    
+    def dequeue(self):
+        pass
+    
+def main():
+    queue = QueueSLL()
+    
+if __name__ == "__main__":
+    main()
