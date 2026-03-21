f978246984d64d580ca390a5fa74cdebd7f76565
sp_3141 <07shourya.pratap@gmail.com>
2026-03-21 22:04:14 +0530

    Updated DLL.py

---
+++ DLL.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+        
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def add(self, val):
+        pass
+    
+    def insert_after_node(self, target, x):
+        pass
+    
+    def delete_at_position(self, pos):
+        pass
+    
+def main():
+    DLL = DoublyLinkedList()
+    
+if __name__ == "__main__":
+    main()
